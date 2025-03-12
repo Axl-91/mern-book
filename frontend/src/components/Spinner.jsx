73a21931af596dbbe48df0918fab1bfee52c9a22
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-03-12 20:53:34 -0300

    Added Edit/Delete Book pages

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className="animate-ping w-16 h-16 m-8"> </div>
+    <div>
+      <h1> LOADING...</h1>
+      <div className="animate-ping w-16 h-16 m-8"> </div>
+    </div>
   )
 }
 
